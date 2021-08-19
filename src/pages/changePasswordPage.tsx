fcb6cf6283935b0902540f45e4c7b4a7ac93e73d
Jasmina <jasmina.eminovski@gmail.com>
2021-08-19 16:10:34 +0200

    Use  use-form for change password form

--- src/pages/changePasswordPage.tsx
+++ src/pages/changePasswordPage.tsx
@@ -1,37 +1,69 @@
 import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useAppDispatch } from '../state/hooks';
 import { history } from '../_helpers/historyHelper';
 import MainLogo from '../components/shared/MainLogo'
 import { changePassword } from '../state/actions/user.actions';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    currentPassword: yup.string().required('This is required field!'),
+    newPassword: yup.string()
+        .required('This is required field!')
+        .min(5, 'New password must contain minimum 5 characters!'),
+    repeatPassword: yup.string()
+        .required('This is required field!')
+        .min(5, 'New password must contain minimum 5 characters!')
+        .oneOf([yup.ref('newPassword'), null], 'New passwords do not match!')
+});
+
 
 export const ChangePasswordPage = () => {
     const dispatch = useAppDispatch();
-    const currentPassword = useRef<HTMLInputElement>(null);
-    const newPassword = useRef<HTMLInputElement>(null);
-    const repeatPassword = useRef<HTMLInputElement>(null);
-    const [error, setError] = useState('');
 
-    const handleChangePassword = (e: any) => {
-        e.preventDefault();
-        const _current = currentPassword.current?.value;
-        const _new = newPassword.current?.value;
-        const _repeat = repeatPassword.current?.value;
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-        if (_current && _current !== '' && _new && _new !== '' && _repeat && _repeat != ''
-                && _new !== _current && _new === _repeat) {
-                dispatch(changePassword(_current, _new, _repeat));
-        } else {
-            setError('Not valid form!');
-        }
+    const onSubmit = (data: any) => {
+        const current = getValues('currentPassword');
+        const newPass = getValues('newPassword');
+        const repeatPass = getValues('repeatPassword');
+        dispatch(changePassword(current, newPass, repeatPass));
     }
 
     return (
         <div className='wrapper centered'>
-			<MainLogo altImg='Login' clickHandler={() => history.push('/')}/>
-			<div className='centered-content-wrap'>
-				<div className='centered-block'>
-					<h1>Change password</h1>
-					<ul>
+            <MainLogo altImg='Login' clickHandler={() => history.push('/')} />
+            <div className='centered-content-wrap'>
+                <div className='centered-block'>
+                    <h1>Change password</h1>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <ul>
+                            <li>
+                                <input {...register('currentPassword')} type='password' placeholder='Current password' className='in-text large' />
+                                <p>{errors.currentPassword?.message}</p>
+                            </li>
+                            <li>
+                                <input {...register('newPassword')} type='password' placeholder='New password' className='in-text large' />
+                                <p>{errors.newPassword?.message}</p>
+                            </li>
+                            <li>
+                                <input {...register('repeatPassword')} type='password' placeholder='Reperat new password' className='in-text large' />
+                                <p>{errors.repeatPassword?.message}</p>
+                            </li>
+                            <li className='right'>
+                                <input type='submit' className='btn blue' value='Change password'/>
+                            </li>
+                        </ul>
+
+
+
+
+
+                    </form>
+                    {/* <ul>
 						<li>
 							<input type='password' placeholder='Current password' className='in-text large' 
                             ref={currentPassword} onChange={() => setError('')}/>
@@ -50,9 +82,9 @@ export const ChangePasswordPage = () => {
 						<li className='right'>
 							<a href=' ' className='btn blue' onClick={handleChangePassword}>Change password</a>
 						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
+					</ul> */}
+                </div>
+            </div>
+        </div>
     )
 }
